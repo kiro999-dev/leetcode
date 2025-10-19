5f382e9b3ba41151edea91b5163be95afaa21967
Zakaria Khourbach <zkhourba@e1r12p1.1337.ma>
2025-10-19 04:07:55 +0100

    adding more solutions

--- day01/Top_K_Frequent_Elements.cpp
+++ day01/Top_K_Frequent_Elements.cpp
@@ -3,8 +3,8 @@
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
-//my first solution so bad o(m*k)
-class Solution {
+// my first solution so bad o(n *k)
+ class Solution {
 
     public:
         vector<int> topKFrequent(vector<int>& nums, int k)
@@ -28,8 +28,44 @@ class Solution {
                 i++;
 
            }
-           
+
             return res;
         }
     };
+// my second solution after neetcode vd so good o(n)
+class Solution2 {
+    public:
+        vector<int> topKFrequent(vector<int>& nums, int k) {
+            unordered_map<int, int> freq;
+            for (int n : nums)
+                freq[n]++;
+    
+            
+            vector<vector<int>> bucket(nums.size() + 1);
+            for (auto& p : freq)
+                bucket[p.second].push_back(p.first);
+    
+            vector<int> result;
+            for (int i = bucket.size() - 1; i >= 0 && result.size() < k; i--) {
+                for (int num : bucket[i]) {
+                    result.push_back(num);
+                    if (result.size() == k)
+                        break;
+                }
+            }
+            return result;
+        }
+    };
 
+int main()
+{
+    vector<int> nums = {1, 2, 1, 2, 1, 2, 3, 1, 3, 2};
+    vector<int> res;
+    Solution2 t;
+    res = t.topKFrequent(nums, 2);
+    for (int nums : res)
+    {
+        cout << nums << ",";
+    }
+    cout << endl;
+}
